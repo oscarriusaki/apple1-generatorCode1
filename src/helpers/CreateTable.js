8599bfb6962693bc396c62dc24a2ce3e3fd2a3fe
Oscar Laura <oscarriusaki@gmail.com>
2023-03-07 16:20:13 -0400

    last save

--- src/helpers/CreateTable.js
+++ src/helpers/CreateTable.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 export const CreateTable = (data, nombre, data2) => {
 
-    let nombreTabla = nombre.trim().replace(/\s+/g, ' ').split(' ')
+    let nombreTabla = nombre.trim().replace(/\s+/g, ' ').split(' ');
     nombreTabla.map(resp => {
         return resp.toLowerCase();
     })
-    nombreTabla = nombreTabla.join('_');
+    nombreTabla = nombreTabla.join('_').toLowerCase();
+    // nombreTabla = nombreTabla;
     let createSqlInicio = ``;
     let createSqlFin = ``;
     for (const key in data2.inputForm) {
@@ -16,11 +17,20 @@ export const CreateTable = (data, nombre, data2) => {
 
         }else if(data.inputForm[key].trim().slice(0,3) === 'id_'){
 
-            createSqlInicio += `${data.inputForm[key].trim()} integer not null,\n`;
+            createSqlInicio += `${data.inputForm[key].trim()} ${data2.inputForm[key].trim()} not null,\n`;
             createSqlFin += `constraint fk_${data.inputForm[key].trim()} foreign key (${data.inputForm[key].trim()}) references ${data.inputForm[key].trim().slice(3)}(${data.inputForm[key].trim()}),\n`;
-        }else if(data.inputForm[key].trim() === `correo_${nombreTabla}`|| /* falta para numero de serie de producto  */
-
-                data.inputForm[key].trim() === `email_${nombreTabla}`){   /* y otros columnas unicas */
+        }else if((data.inputForm[key].trim() === `correo_${nombreTabla}`|| /* falta para numero de serie de producto  */
+            data.inputForm[key].trim() === `correo`||
+            data.inputForm[key].trim() === `email`||
+            data.inputForm[key].trim() === `email_${nombreTabla}`)&&(
+                nombreTabla === 'user' || 
+                nombreTabla === 'usuario'||
+                nombreTabla === 'employee'||
+                nombreTabla === 'empleado'||
+                nombreTabla === 'administrator'||
+                nombreTabla === 'administrador'
+                
+                )){   /* y otros columnas unicas */
             createSqlInicio += `${data.inputForm[key].trim()} varchar(255) not null,\n`;
             createSqlFin = createSqlFin + `constraint unique_${data.inputForm[key].trim()} unique(${data.inputForm[key].trim()}),\n`;
 
